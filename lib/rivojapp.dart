4c659441efb86bcd36a2e9ba9920949bff821c56
shceo <fayzullojalolov42@gmail.com>
2024-07-14 19:23:22 +0500

    upd

--- lib/rivojapp.dart
+++ lib/rivojapp.dart
@@ -1,17 +1,28 @@
-import 'package:edu/assets/constatnts/common_assets.dart';
+
 import 'package:edu/src/ui/theme/app_colors.dart';
+import 'package:edu/src/ui/theme/theme_extensions.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class App extends StatelessWidget {
+
+
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      // color: ,
-      decoration: const BoxDecoration(
-        
+      decoration: BoxDecoration(
+        color: context.theme.whiteColor,
+      ),
+      child: IconButton(
+       icon: Icon(Icons.abc_outlined),
+        onPressed: () {},
       ),
     );
   }
-}
+
+ 
+    }
+  
+
