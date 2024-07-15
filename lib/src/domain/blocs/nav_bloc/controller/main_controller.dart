d94326f5a80fc8b3007d9732b8f262a3fc4a7299
shceo <fayzullojalolov42@gmail.com>
2024-07-15 20:14:00 +0500

    upd

--- lib/src/domain/blocs/nav_bloc/controller/main_controller.dart
+++ lib/src/domain/blocs/nav_bloc/controller/main_controller.dart
@@ -1,6 +1,9 @@
-
 import 'package:edu/assets/constants/common_assets.dart';
+import 'package:edu/src/ui/pages/navbar_pages/darslar_page.dart';
 import 'package:edu/src/ui/pages/navbar_pages/kurslar_page.dart';
+import 'package:edu/src/ui/pages/navbar_pages/profil_page.dart';
+import 'package:edu/src/ui/pages/navbar_pages/reyting_page.dart';
+import 'package:edu/src/ui/pages/navbar_pages/xabarlar_page.dart';
 import 'package:flutter/cupertino.dart';
 
 import '../../../entity/nav_item_entity.dart';
@@ -8,13 +11,20 @@ import '../../../entity/nav_item_entity.dart';
 class MainController {
   List<NavItemEntity> navItems = [
     NavItemEntity(
-        page: const CoursesPage(), icon: CommonAssets.courses, title: 'Kurslar'),
+        page: const CoursesPage(),
+        icon: CommonAssets.courses,
+        title: 'Kurslar'),
     NavItemEntity(
-        page: Container(), icon: CommonAssets.lessons, title: 'Darslar'),
+        page: const LessonsPage(),
+        icon: CommonAssets.lessons,
+        title: 'Darslar'),
     NavItemEntity(
-        page: Container(), icon: CommonAssets.rating, title: 'Reyting'),
+        page: const RatingPage(), icon: CommonAssets.rating, title: 'Reyting'),
     NavItemEntity(
-        page: Container(), icon: CommonAssets.messages, title: 'Xabarlar'),
-    NavItemEntity(page: Container(), icon: CommonAssets.user, title: 'Profil'),
+        page: const MessagesPage(),
+        icon: CommonAssets.messages,
+        title: 'Xabarlar'),
+    NavItemEntity(
+        page: const ProfilePage(), icon: CommonAssets.user, title: 'Profil'),
   ];
 }
---
+++ lib/src/ui/pages/navbar_pages/darslar_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LessonsPage extends StatelessWidget {
+  const LessonsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
