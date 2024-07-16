dcb3ca46a5786993d98dfe12ddf648ae4a0f175c
shceo <fayzullojalolov42@gmail.com>
2024-07-16 12:15:35 +0500

    upd

--- lib/src/ui/pages/screens/auth/presentation/views/sing_in.dart
+++ lib/src/ui/pages/screens/auth/presentation/views/sing_in.dart
@@ -1,10 +1,15 @@
-
 import 'package:edu/assets/constants/common_assets.dart';
+import 'package:edu/src/ui/pages/home_page.dart';
+import 'package:edu/src/ui/pages/routes/app_navigator.dart';
+import 'package:edu/src/utils/constants/common_dimensions.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:go_router/go_router.dart';
 import '../widgets/formatters.dart';
 import '../widgets/text_feild_custom.dart';
 import '../widgets/w_button.dart';
 import 'forgot_password.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SignInView extends StatelessWidget {
   SignInView({super.key});
@@ -15,29 +20,53 @@ class SignInView extends StatelessWidget {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: Padding(
-        padding:  const EdgeInsets.symmetric(horizontal: (25),vertical: (70)),
+        padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 65),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
-              padding: EdgeInsets.symmetric(horizontal:(65)),
-              child: Image.asset(CommonAssets.rivojLogo,),
-            ),
-            SizedBox(height:(10),),
-            Center(
-              child: Text("Akkauntga kirish",style: TextStyle(
-                color: Colors.black,
-                fontSize: 26,),
+              padding: const EdgeInsets.symmetric(horizontal: (55)),
+              child: Image.asset(
+                CommonAssets.rivojLogo,
+                // width: 255,
+                // height: 99,
               ),
             ),
-            SizedBox(height:(37),),
-            Text("Telefon raqam",style: TextStyle(
-              color: Colors.black,
-              fontSize: 18,
-              fontWeight: FontWeight.w500
-            ),),
-            SizedBox(height: (8),),
+            const SizedBox(
+              height: (10),
+            ),
+            Center(
+              child: Text(
+                "Akkauntga kirish",
+                style: GoogleFonts.archivo(
+                  textStyle: const TextStyle(
+                    color: Colors.black,
+                    fontSize: CommonDimensions.largeText,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: (37),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 12),
+              child: Text(
+                "Telefon raqam",
+                style: GoogleFonts.archivo(
+                  textStyle: const TextStyle(
+                    color: Colors.black,
+                    fontSize: CommonDimensions.defaultTextSize,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
             TextFieldCustom(
               formKey: _formKey,
               title: "Telefon raqamingizni kiriting",
@@ -46,36 +75,61 @@ class SignInView extends StatelessWidget {
               maskTextInputFormatter: Formatters.phoneFormatter,
               focusNode: null,
             ),
-            SizedBox(height:(30),),
-            Text("Parol",style: TextStyle(
-                color: Colors.black,
-                fontSize: 18,
-                fontWeight: FontWeight.w500
-            ),),
-            SizedBox(height:(8),),
+            const SizedBox(
+              height: (30),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 12),
+              child: Text(
+                "Parol",
+                style: GoogleFonts.archivo(
+                  textStyle: const TextStyle(
+                    color: Colors.black,
+                    fontSize: CommonDimensions.defaultTextSize,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: (8),
+            ),
             TextFieldCustom(
               formKey: _formKey,
-              title: "",
+              title: 'Parol',
               isPassword: true,
               keyBoardType: TextInputType.text,
               maskTextInputFormatter: null,
               focusNode: null,
             ),
             const Spacer(),
-            WButton(onTap: (){},text: 'Kirish',textStyle: TextStyle(fontSize: 25),),
-            SizedBox(height: (32),),
+            WButton(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => HomePage(),
+                  ),
+                );
+              },
+              text: 'Kirish',
+              textStyle: const TextStyle(fontSize: 25),
+            ),
+            const SizedBox(
+              height: (32),
+            ),
             Center(
               child: TextButton(
-                  onPressed: (){
-                    Navigator.of(context).push(MaterialPageRoute(builder: (_)=> const ForgotPassword()));
-                  }
-                  , child : Text("Parolni unutdingizmi ?",
-                style: TextStyle(fontSize: 20,color: Colors.blue),
-                textAlign: TextAlign.center,
-              )
-              ),
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (_) => const ForgotPassword()));
+                  },
+                  child: const Text(
+                    "Parolni unutdingizmi ?",
+                    style: TextStyle(fontSize: 20, color: Colors.blue),
+                    textAlign: TextAlign.center,
+                  )),
             )
-
           ],
         ),
       ),
--- lib/src/utils/constants/common_dimensions.dart
+++ lib/src/utils/constants/common_dimensions.dart
@@ -6,12 +6,20 @@ abstract final class CommonDimensions {
   static const double medium = 8;
   static const double superLarge = 60;
 
+// text sizes 
+  static const double commonTextSize = 16;
+  static const double defaultTextSize = 18;
+
+  static const double extraLargeText = 20;
+  static const double largeText = 30;
+
+
+// button sizes
+  static const double bwidth = 380;
+  static const double bheight = 52;
+
   // ScreenSize
   static const double maxPhoneWidth = 500;
-  static const double minDurationProgramWidth = 480;
-
-  // Category assets sizes
-  static const double categoryAssetsHeight = 154;
 
   // Padding
   static const double commonPadding = 22;
