005187cc8a315942ef61d435ea09f2cdfa1aea1d
shceo <fayzullojalolov42@gmail.com>
2024-07-18 08:02:18 +0500

    upd

--- lib/src/ui/pages/screens/auth/presentation/views/sing_in.dart
+++ lib/src/ui/pages/screens/auth/presentation/views/sing_in.dart
@@ -3,6 +3,7 @@ import 'package:edu/src/domain/api/auth_swagger/auth_api.dart';
 import 'package:edu/src/domain/api/data/auth_model.dart';
 import 'package:edu/src/ui/pages/home_page.dart';
 import 'package:edu/src/ui/theme/app_themes.dart';
+import 'package:edu/src/utils/constants/common_dimensions.dart';
 import 'package:edu/src/utils/size/size.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -10,7 +11,6 @@ import 'package:flutter/services.dart';
 
 import '../widgets/formatters.dart';
 import '../widgets/text_feild_custom.dart';
-import '../widgets/w_button.dart';
 import 'forgot_password.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -53,20 +53,27 @@ class _SignInViewState extends State<SignInView> {
               Center(
                 child: Text(
                   "Akkauntga kirish",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 26.w,
+                  style: GoogleFonts.archivo(
+                    textStyle: const TextStyle(
+                      color: Colors.black,
+                      fontSize: CommonDimensions.largeText,
+                      fontWeight: FontWeight.w700,
+                    ),
                   ),
                 ),
               ),
               37.getH(),
-              Text(
-                "Telefon raqam",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 18.w,
-                  fontWeight: FontWeight.w500,
+              Padding(
+                padding: const EdgeInsets.only(left: CommonDimensions.medium),
+                child: Text(
+                  "Telefon raqam",
+                  style: GoogleFonts.archivo(
+                    textStyle: const TextStyle(
+                      color: Colors.black,
+                      fontSize: CommonDimensions.defaultTextSize,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
                 ),
               ),
               8.getH(),
@@ -80,12 +87,17 @@ class _SignInViewState extends State<SignInView> {
                 focusNode: null,
               ),
               30.getH(),
-              Text(
-                "Parol",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 18.w,
-                  fontWeight: FontWeight.w500,
+              Padding(
+                padding: const EdgeInsets.only(left: CommonDimensions.medium),
+                child: Text(
+                  "Parol",
+                  style: GoogleFonts.archivo(
+                    textStyle: const TextStyle(
+                      color: Colors.black,
+                      fontSize: CommonDimensions.defaultTextSize,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
                 ),
               ),
               8.getH(),
@@ -101,7 +113,6 @@ class _SignInViewState extends State<SignInView> {
               const Spacer(),
               GestureDetector(
                 onTap: () {
-                  print('');
                   if (_passwordController.text.isNotEmpty &&
                       _phoneController.text.isNotEmpty) {
                     debugPrint("ONTAP BUTTON");
@@ -114,7 +125,7 @@ class _SignInViewState extends State<SignInView> {
                         MaterialPageRoute(builder: (_) => HomePage()),
                       );
                     });
-                  } 
+                  }
                   // else {
                   //   Fluttertoast.showToast(msg: 'Такого пользователя нет');
                   // }
