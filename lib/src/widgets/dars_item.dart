04d9e0b2a8f2ba3e7459f305defbdc586c2eafc7
shceo <fayzullojalolov42@gmail.com>
2024-07-18 08:17:31 +0500

    up

--- lib/src/widgets/dars_item.dart
+++ lib/src/widgets/dars_item.dart
@@ -2,7 +2,8 @@ import 'package:edu/src/ui/theme/app_themes.dart';
 import 'package:flutter/material.dart';
 
 class DarsItem extends StatelessWidget {
-  const DarsItem({super.key, required this.tap, required this.onTap, required this.text});
+  const DarsItem(
+      {super.key, required this.tap, required this.onTap, required this.text});
   final bool tap;
   final VoidCallback onTap;
   final String text;
@@ -15,17 +16,15 @@ class DarsItem extends StatelessWidget {
         margin: const EdgeInsets.only(right: 7),
         padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 3),
         decoration: BoxDecoration(
-          color: tap ? blue : Colors.white,
-          borderRadius: BorderRadius.circular(10),
-          border: Border.all(
-            color : grey,
-            width: tap ? 0 :1
-          )
-        ),
+            color: tap ? blue : Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(color: grey, width: tap ? 0 : 1)),
         child: Text(
           text,
           style: TextStyle(
-              color: tap ? Colors.white : Colors.black, fontSize: 15, fontWeight: FontWeight.w700),
+              color: tap ? Colors.white : Colors.black,
+              fontSize: 15,
+              fontWeight: FontWeight.w700),
         ),
       ),
     );
