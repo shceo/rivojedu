d722a9480a521657bd759afdca81ce41e23c904a
shceo <fayzullojalolov42@gmail.com>
2024-08-16 00:31:17 +0500

    sk upd

--- lib/src/ui/pages/navbar_pages/profil_page.dart
+++ lib/src/ui/pages/navbar_pages/profil_page.dart
@@ -5,6 +5,7 @@ import 'package:edu/src/widgets/edit_password_item.dart';
 import 'package:edu/src/widgets/image_dialog.dart';
 import 'package:edu/src/widgets/password_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:url_launcher/url_launcher.dart';
 
